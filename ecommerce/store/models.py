cffea5f40324d68424f5a60f7ef4afc24aa54913
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-02 23:32:27 +0530

    Customer Model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name
