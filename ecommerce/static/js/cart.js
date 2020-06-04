44267d94b8196f4f248cd9ae55c81a06a91a2245
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-04 23:56:32 +0530

    Add to Cart

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,16 +1,30 @@
-var updateBtns = document.getElementsByClassName("update-cart");
+var updateBtns = document.getElementsByClassName('update-cart');
 
 for (var i = 0; i < updateBtns.length; i++) {
-  updateBtns[i].addEventListener("click", function () {
+  updateBtns[i].addEventListener('click', function () {
     var productId = this.dataset.product;
     var action = this.dataset.action;
-    console.log("ProductId:", productId, "Action:", action);
+    console.log('ProductId:', productId, 'Action:', action);
 
-    console.log("User", user);
-    if (user == "AnonymousUser") {
-      console.log("Not Logged In");
+    console.log('User', user);
+    if (user == 'AnonymousUser') {
+      console.log('Not Logged In');
     } else {
-      console.log("Logged In,Sending data....");
+      updateUserOrder(productId, action);
     }
   });
+}
+
+function updateUserOrder(productId, action) {
+  var url = '/update_item/';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': csrftoken,
+    },
+    body: JSON.stringify({ productId, action }),
+  })
+    .then((response) => response.json())
+    .then((data) => location.reload());
 }
