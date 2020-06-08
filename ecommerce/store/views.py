67c6a062883376afc8b746587d911fb92b5e6bea
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-09 00:18:00 +0530

    Clean-up ProductOrder View

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from .models import *
 import json
 import datetime
-from .utils import cartData, cookieCart
+from .utils import cartData, cookieCart,guestOrder
 
 # Create your views here.
 
@@ -76,28 +76,8 @@ def processOrder(request):
             customer=customer, complete=False)
 
     else:
-        print("User is not logged in")
-
-        print("Cookies:", request.COOKIES)
-
-        name = data['form']['name']
-        email = data['form']['email']
-
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create(email=email)
-        customer.name = name
-        customer.save()
-
-        order = Order.objects.create(customer=customer, complete=False)
-
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-
-            orderItem = OrderItem.objects.create(
-                product=product, order=order, quantity=item['quantity'])
-
+        customer,order=guestOrder(request,data)
+       
     total = (data['form']['total'])
     order.transaction_id = transaction_id
     if total == order.get_cart_items:
