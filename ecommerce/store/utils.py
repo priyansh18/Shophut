98996fb79c71b6291d1ae741525aab7a38e612f4
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-08 21:46:48 +0530

    Copied Cart Logic

--- ecommerce/store/utils.py
+++ ecommerce/store/utils.py
@@ -1,5 +1,41 @@
 import json
 from .models import *
 
+
 def cookieCart(request):
-  return {}
+    try:
+        cart = json.loads(request.COOKIES['cart'])
+    except:
+        cart = {}
+
+    items = []
+    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+    cartItems = order['get_cart_total']
+
+    for i in cart:
+        try:
+            cartItems += cart[i]['quantity']
+
+            product = Product.objects.get(id=i)
+            total = (product.price*cart[i]['quantity'])
+
+            order['get_cart_items'] += total
+            order['get_cart_total'] += cart[i]['quantity']
+
+            item = {
+                'product': {
+                    'id': product.id,
+                    'name': product.name,
+                    'price': product.price,
+                    'image': product.image,
+                },
+                'quantity': cart[i]['quantity'],
+                'get_total': total
+            }
+            items.append(item)
+
+            if(product.digital == False):
+                order['shipping'] = True
+        except:
+            pass
+    return {'items': items, 'order': order, 'cartItems': cartItems}
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -34,42 +34,7 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_total
     else:
-        try:
-            cart = json.loads(request.COOKIES['cart'])
-        except:
-            cart = {}
-        print('Cart', cart)
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping':False}
-        cartItems = order['get_cart_total']
-
-        for i in cart:
-            try:
-                cartItems += cart[i]['quantity']
-
-                product = Product.objects.get(id=i)
-                total = (product.price*cart[i]['quantity'])
-
-                order['get_cart_items'] += total
-                order['get_cart_total'] += cart[i]['quantity']
-
-                item = {
-                    'product': {
-                        'id': product.id,
-                        'name': product.name,
-                        'price': product.price,
-                        'image': product.image,
-                    },
-                    'quantity': cart[i]['quantity'],
-                    'get_total': total
-                }
-                items.append(item)
-
-                if(product.digital==False):
-                    order['shipping']= True
-            except:
-                pass
-
+        
 
     context = {'items': items, 'order': order,
                'cartItems': cartItems, 'shipping': False}
