8ef1a724581536b57eb384e2d098717382453b49
priyansh18 <singhalpriyansh58@gmail.com>
2021-07-16 19:58:33 +0530

    MIME Type error fixed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import json
 import os
 from django.core.exceptions import ImproperlyConfigured
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
